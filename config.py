d75bd39b3b77de9c1d97e4e7a9cc4dbdec863fc8
Franklin <Franklinkreleger@gmail.com>
2023-09-27 18:36:53 +0100

    modified:   config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 I2C_ADDRESS = 0x76  # BME280
 MESSAGE_TIMESPAN = 2000  # Milliseconds
-SIMULATED_DATA = False
+SIMULATED_DATA = True
 # Put your Device Connection String in here
 # Example: 'HostName=iothub.azure-devices.net;DeviceId=raspberry;SharedAccessKey=VGhpcyBpcyBub3QgYWN0dWFsbHkgYSBrZXkgOik='
 IOTHUB_DEVICE_CONNECTION_STRING='HostName=FreekHub.azure-devices.net;DeviceId=rasp;SharedAccessKey=fHIIUwn+6gQOkAcLYS59gLWBd43BZ5ge/GPfbsswQH4='
